a437f9a2c0adfae9aede6846b4a93d0a258def8f
vibhu88 <vibhu.eclipse@gmail.com>
2020-07-06 18:46:16 +0530

    Modified to introduce Global styles

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import {globalStyles} from '../styles/global';
 
 export default function Home() {
     return (
-        <View style = {styles.container}>
+        <View style = {globalStyles.container}>
             <Text>Review Details Screen</Text>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        padding: 24
-    }
-})
