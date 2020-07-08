620c94dd0d4d1dfa29d4069ff5a55a700edb2b52
vibhu88 <vibhu.eclipse@gmail.com>
2020-07-08 21:02:57 +0530

    Added Modal

--- screens/home.js
+++ screens/home.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity, Modal } from 'react-native';
 import {globalStyles} from '../styles/global';
 import Card from '../shared/card';
+import { MaterialIcons} from '@expo/vector-icons';
 
 export default function Home({ navigation }) {
+
+    const [modalOpen, setModalOpen] = useState(false);
 
     const [reviews, setReviews] = useState([
         {title: 'Title 1', rating: 5, body: 'Body 1', key: '1'},
@@ -13,6 +16,26 @@ export default function Home({ navigation }) {
 
     return (
         <View style = {globalStyles.container}>
+
+            <Modal visible={modalOpen} animationType='slide'>
+                <View style={styles.modalContent}>
+                    <MaterialIcons
+                     name='close'
+                     size={24}
+                     style={{ ...styles.modalToggle, ...styles.modalClose}}
+                     onPress={() => setModalOpen(false)}
+                     />
+                     <Text> Hello from Modal </Text>
+                </View>
+            </Modal>
+
+            <MaterialIcons
+                     name='add'
+                     size={24}
+                     style={styles.modalToggle}
+                     onPress={() => setModalOpen(true)}
+            />
+
             <FlatList 
                 data={reviews}
                 renderItem={({ item }) => (
@@ -26,3 +49,21 @@ export default function Home({ navigation }) {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    modalToggle: {
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: '#f2f2f2',
+        padding: 10,
+        borderRadius: 10,
+        alignSelf: 'center'
+    },
+    modalClose: {
+        marginTop: 20,
+        marginBottom: 0
+    },
+    modalContent: {
+        flex: 1
+    }
+})
